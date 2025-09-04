7b1ef56ded7cd5c048ea01898c21c7709d6827da
WillmanGZ <willmang@uninorte.edu.co>
2025-09-03 23:11:32 -0500

    feat: adjust style

--- Frontend/src/js/dashboard.js
+++ Frontend/src/js/dashboard.js
@@ -24,7 +24,7 @@ async function getAlerts() {
 
           // Only show type, severity, and username
           article.innerHTML = `
-            <button class="font-bold text-orange-600 hover:size-1 absolute left-0 top-0" onclick="deleteAlert('${route._id}', '${alert._id}')">X</button>
+            <button class="font-bold text-orange-600 hover:size-1 absolute right-0 top-0" onclick="deleteAlert('${route._id}', '${alert._id}')">X</button>
             <h4 class="font-semibold text-lg text-gray-800">${alert.type}</h4>
             <p class="text-sm text-gray-500">Route: ${route.name}</p>
             <p class="text-md font-bold text-orange-600">Severity: ${alert.severity}</p>
